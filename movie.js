ead5776b18f451a33f508cf5bd5b2b1334f163ad
gilbert <gilbert.bageni@student.moringaschool.com>
2023-10-12 13:31:52 +0300

    code updated

--- movie.js
+++ movie.js
@@ -1,15 +1,3 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
-
 document.addEventListener("DOMContentLoaded", () => {
   let selectedMovieId = localStorage.getItem("selectedMovieId");
 
